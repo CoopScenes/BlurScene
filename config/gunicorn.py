795d93bd6f2f2156a4c55e9b901ff01da2e2b07d
Florian König <florian@elara-0>
2025-03-04 16:02:08 +0100

    config bugfix

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -2,7 +2,7 @@ from os import environ as env
 
 workers = env.get("WORKERS") or 1
 timeout = env.get("WORKER_TIMEOUT") or 120
-bind = '0.0.0.0:' + (env.get("WORKERS") or "5000")
+bind = '0.0.0.0:' + (env.get("PORT") or "5000")
 loglevel = str.lower(env.get("LOG_LEVEL") or 'debug')
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
