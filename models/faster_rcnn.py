ff78f426b3352c67ef85ce0741256e6623b0d860
Florian König <florian@elara-0>
2025-03-05 18:17:49 +0100

    comment out unused monkey patches

--- models/faster_rcnn.py
+++ models/faster_rcnn.py
@@ -181,199 +181,199 @@ class FasterRCNNWrapper(ModuleAdapter):
 ### monkey patches / some replacements for torchvision functions
 ###
 
-#
-# RegionProposalNetwork with DIoU and Focal Losses
-#
+# #
+# # RegionProposalNetwork with DIoU and Focal Losses
+# #
 
-OriginalRegionProposalNetwork = tmdf.RegionProposalNetwork
+# OriginalRegionProposalNetwork = tmdf.RegionProposalNetwork
 
-class RegionProposalNetwork(OriginalRegionProposalNetwork):
-    def compute_loss(
-            self,
-            objectness: torch.Tensor,
-            proposals: torch.Tensor, # [B,N,4]
-            labels: list[torch.Tensor], # list is batch dimension, tensor [N_B,]
-            gt_bboxes: list[torch.Tensor] # list is batch dimension, tensor [N_B,4]
-        ) -> tuple[torch.Tensor, torch.Tensor]:
-        """
-        torchvision.models.detection.rpn.compute_loss with DIoU and focal losses.
-        """
-        sampled_pos_inds, sampled_neg_inds = self.fg_bg_sampler(labels)
-        sampled_pos_inds = torch.where(torch.cat(sampled_pos_inds, dim=0))[0]
-        sampled_neg_inds = torch.where(torch.cat(sampled_neg_inds, dim=0))[0]
+# class RegionProposalNetwork(OriginalRegionProposalNetwork):
+#     def compute_loss(
+#             self,
+#             objectness: torch.Tensor,
+#             proposals: torch.Tensor, # [B,N,4]
+#             labels: list[torch.Tensor], # list is batch dimension, tensor [N_B,]
+#             gt_bboxes: list[torch.Tensor] # list is batch dimension, tensor [N_B,4]
+#         ) -> tuple[torch.Tensor, torch.Tensor]:
+#         """
+#         torchvision.models.detection.rpn.compute_loss with DIoU and focal losses.
+#         """
+#         sampled_pos_inds, sampled_neg_inds = self.fg_bg_sampler(labels)
+#         sampled_pos_inds = torch.where(torch.cat(sampled_pos_inds, dim=0))[0]
+#         sampled_neg_inds = torch.where(torch.cat(sampled_neg_inds, dim=0))[0]
 
-        sampled_inds = torch.cat([sampled_pos_inds, sampled_neg_inds], dim=0)
+#         sampled_inds = torch.cat([sampled_pos_inds, sampled_neg_inds], dim=0)
 
-        objectness = objectness.flatten()
+#         objectness = objectness.flatten()
 
-        labels = torch.cat(labels, dim=0)
-        gt_bboxes = torch.cat(gt_bboxes, dim=0)
-        proposals = proposals.view(-1, 4)
+#         labels = torch.cat(labels, dim=0)
+#         gt_bboxes = torch.cat(gt_bboxes, dim=0)
+#         proposals = proposals.view(-1, 4)
 
-        box_loss = complete_box_iou_loss(
-            proposals[sampled_pos_inds],
-            gt_bboxes[sampled_pos_inds],
-            reduction="sum"
-        ) / (sampled_inds.numel())
-        # originally:
-        # F.smooth_l1_loss(
-        #     pred_bbox_deltas[sampled_pos_inds],
-        #     regression_targets[sampled_pos_inds],
-        #     beta=1 / 9,
-        #     reduction="sum",
-        # ) / (sampled_inds.numel())
+#         box_loss = complete_box_iou_loss(
+#             proposals[sampled_pos_inds],
+#             gt_bboxes[sampled_pos_inds],
+#             reduction="sum"
+#         ) / (sampled_inds.numel())
+#         # originally:
+#         # F.smooth_l1_loss(
+#         #     pred_bbox_deltas[sampled_pos_inds],
+#         #     regression_targets[sampled_pos_inds],
+#         #     beta=1 / 9,
+#         #     reduction="sum",
+#         # ) / (sampled_inds.numel())
 
-        objectness_loss = sigmoid_focal_loss(
-            objectness[sampled_inds],
-            labels[sampled_inds],
-            alpha=-1,
-            gamma=2,
-            reduction="sum"
-        ) / (sampled_inds.numel())
-        # originally:
-        # F.binary_cross_entropy_with_logits(objectness[sampled_inds], labels[sampled_inds])
+#         objectness_loss = sigmoid_focal_loss(
+#             objectness[sampled_inds],
+#             labels[sampled_inds],
+#             alpha=-1,
+#             gamma=2,
+#             reduction="sum"
+#         ) / (sampled_inds.numel())
+#         # originally:
+#         # F.binary_cross_entropy_with_logits(objectness[sampled_inds], labels[sampled_inds])
 
-        return objectness_loss, box_loss
+#         return objectness_loss, box_loss
 
-    def forward(
-        self,
-        images: ImageList,
-        features: dict[str, torch.Tensor],
-        targets: Optional[list[dict[str, torch.Tensor]]] = None,
-    ) -> tuple[list[torch.Tensor], dict[str, torch.Tensor]]:
+#     def forward(
+#         self,
+#         images: ImageList,
+#         features: dict[str, torch.Tensor],
+#         targets: Optional[list[dict[str, torch.Tensor]]] = None,
+#     ) -> tuple[list[torch.Tensor], dict[str, torch.Tensor]]:
 
-        """
-        Copy of torchvision.models.detection.rpn.RegionProposalNetwork.forward
-        adapted for different losses.
-        """
-        features = list(features.values())
-        objectness, pred_bbox_deltas = self.head(features)
-        anchors = self.anchor_generator(images, features)
+#         """
+#         Copy of torchvision.models.detection.rpn.RegionProposalNetwork.forward
+#         adapted for different losses.
+#         """
+#         features = list(features.values())
+#         objectness, pred_bbox_deltas = self.head(features)
+#         anchors = self.anchor_generator(images, features)
 
-        num_images = len(anchors)
-        num_anchors_per_level_shape_tensors = [o[0].shape for o in objectness]
-        num_anchors_per_level = [
-            s[0] * s[1] * s[2] for s in num_anchors_per_level_shape_tensors
-        ]
-        objectness, pred_bbox_deltas = concat_box_prediction_layers(
-            objectness,
-            pred_bbox_deltas
-        )
+#         num_images = len(anchors)
+#         num_anchors_per_level_shape_tensors = [o[0].shape for o in objectness]
+#         num_anchors_per_level = [
+#             s[0] * s[1] * s[2] for s in num_anchors_per_level_shape_tensors
+#         ]
+#         objectness, pred_bbox_deltas = concat_box_prediction_layers(
+#             objectness,
+#             pred_bbox_deltas
+#         )
 
-        proposals = self.box_coder.decode(pred_bbox_deltas, anchors)
-        proposals = proposals.view(num_images, -1, 4)
-        boxes, scores = self.filter_proposals(
-            proposals.detach(),
-            objectness.detach(),
-            images.image_sizes,
-            num_anchors_per_level
-        )
+#         proposals = self.box_coder.decode(pred_bbox_deltas, anchors)
+#         proposals = proposals.view(num_images, -1, 4)
+#         boxes, scores = self.filter_proposals(
+#             proposals.detach(),
+#             objectness.detach(),
+#             images.image_sizes,
+#             num_anchors_per_level
+#         )
 
-        losses = {}
-        if self.training:
-            if targets is None:
-                raise ValueError("targets should not be None")
-            labels, matched_gt_boxes = self.assign_targets_to_anchors(
-                anchors,
-                targets
-            )
-
-
-            loss_objectness, loss_rpn_box_reg = self.compute_loss(
-                objectness, proposals, labels, matched_gt_boxes
-            )
-            losses = {
-                "loss_objectness": loss_objectness,
-                "loss_rpn_box_reg": loss_rpn_box_reg,
-            }
-        return boxes, losses
-
-tmdf.RegionProposalNetwork = RegionProposalNetwork
-
-#
-# roi_align using improved alignment from detectron2
-#
-
-orig_roi_align = top.roi_align
-
-def detectron_roi_align(*args, **kwargs):
-    if "aligned" not in kwargs:
-        kwargs["aligned"] = True
-    return orig_roi_align(*args, **kwargs)
-
-top.roi_align = detectron_roi_align
-
-#
-# alternative iou for rpn.box_similarity to tame OOMs for many ground truths
-#
-
-@torch.compiler.disable
-def chunked_box_iou(
-        boxes1: torch.Tensor,
-        boxes2: torch.Tensor,
-        chunk_size = 16384 # in isolation 1800 is optimum on 4090, but ultraslow in training
-) -> torch.Tensor:
-    """
-    Copy of torchvision.ops.boxes.box_iou that chunks the IoU
-    computations. Can be used in case of datasets with a huge number
-    of candidate boxes, which frequently lead to OOMs when processed
-    at once.
-    """
-    if sqrt(boxes1.shape[0]*boxes2.shape[0]) < chunk_size:
-        return box_iou(boxes1, boxes2)
-
-    if not torch.jit.is_scripting() and not torch.jit.is_tracing():
-        _log_api_usage_once(chunked_box_iou)
-
-    if boxes2.shape[0] < chunk_size:
-        chunk_y_size = boxes2.shape[0]
-        chunk_x_size = floor(chunk_size / chunk_y_size)
-    elif boxes1.shape[0] < chunk_size:
-        chunk_x_size = boxes1.shape[0]
-        chunk_y_size = floor(chunk_size / chunk_x_size)
-    else:
-        chunk_x_size = chunk_size
-        chunk_y_size = chunk_size
-
-    iou = torch.zeros(
-        (boxes1.shape[0], boxes2.shape[0]),
-        device=boxes1.device,
-        dtype=boxes1.dtype
-    )
-
-    i = 0
-    j = 0
-    while i * chunk_x_size < boxes1.shape[0]:
-        x_slice = slice(i*chunk_x_size, (i+1)*chunk_x_size)
-        while j * chunk_y_size < boxes2.shape[0]:
-            y_slice = slice(j*chunk_y_size, (j+1)*chunk_y_size)
-
-            inter, union = _memopt_box_inter_union(
-                boxes1[x_slice, :],
-                boxes2[y_slice, :]
-            )
-            iou[x_slice, y_slice] = inter / union
-
-            j += 1
-        i += 1
-
-    return iou
+#         losses = {}
+#         if self.training:
+#             if targets is None:
+#                 raise ValueError("targets should not be None")
+#             labels, matched_gt_boxes = self.assign_targets_to_anchors(
+#                 anchors,
+#                 targets
+#             )
 
 
-def _memopt_box_inter_union(
-        boxes1: torch.Tensor,
-        boxes2: torch.Tensor
-) -> tuple[torch.Tensor, torch.Tensor]:
-    # copy of _box_inter_union that shouldn't create superfluous vars
-    wh = _upcast(
-        torch.min(boxes1[:, None, 2:], boxes2[:, 2:]) -
-        torch.max(boxes1[:, None, :2], boxes2[:, :2])
-    ).clamp(min=0)  # [N,M,2]
-    inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
+#             loss_objectness, loss_rpn_box_reg = self.compute_loss(
+#                 objectness, proposals, labels, matched_gt_boxes
+#             )
+#             losses = {
+#                 "loss_objectness": loss_objectness,
+#                 "loss_rpn_box_reg": loss_rpn_box_reg,
+#             }
+#         return boxes, losses
 
-    union = box_area(boxes1)[:, None] + box_area(boxes2) - inter
+# tmdf.RegionProposalNetwork = RegionProposalNetwork
 
-    return inter, union
+# #
+# # roi_align using improved alignment from detectron2
+# #
+
+# orig_roi_align = top.roi_align
+
+# def detectron_roi_align(*args, **kwargs):
+#     if "aligned" not in kwargs:
+#         kwargs["aligned"] = True
+#     return orig_roi_align(*args, **kwargs)
+
+# top.roi_align = detectron_roi_align
+
+# #
+# # alternative iou for rpn.box_similarity to tame OOMs in case of too many ground truths
+# #
+
+# @torch.compiler.disable
+# def chunked_box_iou(
+#         boxes1: torch.Tensor,
+#         boxes2: torch.Tensor,
+#         chunk_size = 16384 # in isolation 1800 is optimum on 4090, but ultraslow in training
+# ) -> torch.Tensor:
+#     """
+#     Copy of torchvision.ops.boxes.box_iou that chunks the IoU
+#     computations. Can be used in case of datasets with a huge number
+#     of candidate boxes, which frequently lead to OOMs when processed
+#     at once.
+#     """
+#     if sqrt(boxes1.shape[0]*boxes2.shape[0]) < chunk_size:
+#         return box_iou(boxes1, boxes2)
+
+#     if not torch.jit.is_scripting() and not torch.jit.is_tracing():
+#         _log_api_usage_once(chunked_box_iou)
+
+#     if boxes2.shape[0] < chunk_size:
+#         chunk_y_size = boxes2.shape[0]
+#         chunk_x_size = floor(chunk_size / chunk_y_size)
+#     elif boxes1.shape[0] < chunk_size:
+#         chunk_x_size = boxes1.shape[0]
+#         chunk_y_size = floor(chunk_size / chunk_x_size)
+#     else:
+#         chunk_x_size = chunk_size
+#         chunk_y_size = chunk_size
+
+#     iou = torch.zeros(
+#         (boxes1.shape[0], boxes2.shape[0]),
+#         device=boxes1.device,
+#         dtype=boxes1.dtype
+#     )
+
+#     i = 0
+#     j = 0
+#     while i * chunk_x_size < boxes1.shape[0]:
+#         x_slice = slice(i*chunk_x_size, (i+1)*chunk_x_size)
+#         while j * chunk_y_size < boxes2.shape[0]:
+#             y_slice = slice(j*chunk_y_size, (j+1)*chunk_y_size)
+
+#             inter, union = _memopt_box_inter_union(
+#                 boxes1[x_slice, :],
+#                 boxes2[y_slice, :]
+#             )
+#             iou[x_slice, y_slice] = inter / union
+
+#             j += 1
+#         i += 1
+
+#     return iou
+
+
+# def _memopt_box_inter_union(
+#         boxes1: torch.Tensor,
+#         boxes2: torch.Tensor
+# ) -> tuple[torch.Tensor, torch.Tensor]:
+#     # copy of _box_inter_union that shouldn't create superfluous vars
+#     wh = _upcast(
+#         torch.min(boxes1[:, None, 2:], boxes2[:, 2:]) -
+#         torch.max(boxes1[:, None, :2], boxes2[:, :2])
+#     ).clamp(min=0)  # [N,M,2]
+#     inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
+
+#     union = box_area(boxes1)[:, None] + box_area(boxes2) - inter
+
+#     return inter, union
 
 
 ###
